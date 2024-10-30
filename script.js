3704f34c89f442ac8885bdbcbc7b7cf5819ca527
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2024-10-30 19:40:36 +0100

    CSS updates

---
+++ script.js
@@ -0,0 +1,9 @@
+const mortgageAmount = document.querySelector("#mortgage-amount");
+const mortgageTerm = document.querySelector("#mortgage-term");
+const interestRate = document.querySelector("#interest-rate");
+
+document.getElementById("submit-button").addEventListener("click", myFunction);
+
+
+
+// repayment, interest only
